a706bbe942b40141053011108d49108811aebb86
Jakia <jtchowdhury27@amherst.edu>
2025-05-05 18:29:22 -0400

    Adding Hash Table

---
+++ src/actions/ds/ll/HashTable.java
@@ -0,0 +1,38 @@
+public class HashTable {
+    public static int TABLE_SIZE = 100; 
+    public static LinkedList[] table = new LinkedList[TABLE_SIZE]; // array of linked lists 
+    public static int size = 0; // number of elements in the hash table
+    public HashTable() {
+        for (int i = 0; i < TABLE_SIZE; i++) {
+            table[i] = new LinkedList(); // initialize each linked list in the array
+        }
+    }
+    public int hash(int key) {
+        double A = (Math.sqrt(5) - 1) / 2 ; // Golden ratio
+        return (int) (TABLE_SIZE * (key * A % 1)); // hash function
+    }
+    public void insert(int key) {
+        int index = hash(key); // get the index for the key
+        table[index].add(key); // add the key to the linked list at that index
+        size++; // increment the size of the hash table
+    }
+    public boolean search(int key) {
+        int index = hash(key); // get the index for the key
+        LLNode node = table[index].find(key); // find the key in the linked list at that index
+        return node != null; // return true if found, false otherwise
+    }
+    public void remove(int key) {
+        int index = hash(key); // get the index for the key
+        table[index].remove(key); // remove the key from the linked list at that index
+        size--; // decrement the size of the hash table
+    }
+    public void printTable() {
+        for (int i = 0; i < TABLE_SIZE; i++) {
+            System.out.print("Index " + i + ": ");
+            table[i].printList(); // print the linked list at that index
+        }
+    }
+    public int getSize() {
+        return size; // return the size of the hash table
+    }
+}
